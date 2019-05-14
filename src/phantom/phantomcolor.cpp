09ee18f83bff5715551ea0aa3e588658af5018a1
cancel <cancel@cancel.fm>
2019-05-14 17:46:55 +0900

    Fix potential floating point divide-by-zero in hsluv-c
    Reported by @jcelerier. This is only a noticeable problem when using
    UBsan, since it will detect floating point division by zero. While this
    is not an error under IEEE floating point rules (it should produce a
    NaN), clang and gcc pretend that they do not officially support IEEE
    floating point in the strict general case so that they can do some
    optimizations. However, they will not exploit floating point division-
    by-zero to do anything nefarious like delete code paths. (This would
    break a lot of software.)
    
    The extra test/branch here is probably only about 20% slower on Core 2
    Duo and this code is not in a hot path, so it's worth fixing if nothing
    else just to stop UBsan warnings for people who use it.

--- src/phantom/phantomcolor.cpp
+++ src/phantom/phantomcolor.cpp
@@ -212,8 +212,16 @@ l2y(double l)
 static void
 xyz2luv(Triplet* in_out)
 {
-    double var_u = (4.0 * in_out->a) / (in_out->a + (15.0 * in_out->b) + (3.0 * in_out->c));
-    double var_v = (9.0 * in_out->b) / (in_out->a + (15.0 * in_out->b) + (3.0 * in_out->c));
+    double divisor = in_out->a + (15.0 * in_out->b) + (3.0 * in_out->c);
+    if(divisor <= 0.00000001) {
+        in_out->a = 0.0;
+        in_out->b = 0.0;
+        in_out->c = 0.0;
+        return;
+    }
+
+    double var_u = (4.0 * in_out->a) / divisor;
+    double var_v = (9.0 * in_out->b) / divisor;
     double l = y2l(in_out->b);
     double u = 13.0 * l * (var_u - ref_u);
     double v = 13.0 * l * (var_v - ref_v);
